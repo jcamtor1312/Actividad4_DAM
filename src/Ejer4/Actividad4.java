15016d9c5bfd4c2fcfb33b89b4ab8b823c1f0f31
jcamtor1312 <163828614+jcamtor1312@users.noreply.github.com>
2024-04-10 10:25:59 +0200

    Comentarios añadidos Actividad4.java

--- src/Ejer4/Actividad4.java
+++ src/Ejer4/Actividad4.java
@@ -2,18 +2,23 @@ package Ejer4;
 
 public class Actividad4
 {
+    // Metodo Main para inicializar Asignaturas
     public static void main(String[] args)
     {
         Asignatura asignatura1 = new Asignatura(1);
         Asignatura asignatura2 = new Asignatura(2);
         Asignatura asignatura3 = new Asignatura(3);
 
+        // Creacion de Alumno con las asignaturas
         Alumno alumno = new Alumno(asignatura1, asignatura2, asignatura3);
 
+        // Creacion de Profesor
         Profesor profesor = new Profesor();
 
-        profesor.ponerNotas(alumno);
+        // Para poner las calificaciones
+        profesor.ponerNotas(alumno);    
 
+        // Hacer media
         double media = profesor.calcularMedia(alumno);
         System.out.println("La media del alumno es: " + media);
     }
